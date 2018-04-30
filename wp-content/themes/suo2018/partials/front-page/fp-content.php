03c8e978e195d8442b03cc8dd7ff34a325b0295f
Sascha Metz <metz@raum3.de>
2018-04-30 22:13:19 +0200

    Initial Frontpage

---
+++ wp-content/themes/suo2018/partials/front-page/fp-content.php
@@ -0,0 +1,26 @@
+<section class="fp-content d-flex flex-wrap text-center mx-auto">
+	<div class="fp-content-square d-flex align-items-center justify-content-center" data-link="bands">
+		<div class="square_inner">
+		    <i class="flaticon bands display-4 d-inline-block mb-1"></i>
+			<h2>Bands</h2>
+		</div>
+	</div>
+	<div class="fp-content-square d-flex align-items-center justify-content-center" data-link="info">
+		<div class="square_inner">
+		    <i class="fa fa-info-circle display-4 d-inline-block mb-1"></i>
+			<h2>Info</h2>
+		</div>
+	</div>
+	<div class="fp-content-square d-flex align-items-center justify-content-center<?php if(!get_field('tickets','options'))echo ' off';?>" data-link="tickets">
+		<div class="square_inner">
+		    <i class="fa fa-ticket display-4 d-inline-block mb-1"></i>
+			<h2>Tickets</h2>
+		</div>
+	</div>
+	<div class="fp-content-square d-flex align-items-center justify-content-center" data-link="anfahrt">
+		<div class="square_inner">
+		    <i class="flaticon anfahrt display-4 d-inline-block mb-1"></i>
+			<h2>Anfahrt</h2>
+		</div>
+	</div>
+</section>
--- wp-content/themes/suo2018/partials/front-page/fp-header.php
+++ wp-content/themes/suo2018/partials/front-page/fp-header.php
@@ -1,3 +1,3 @@
-<section class="fixed-top text-center py-3 py-lg-4">
+<header class="fixed-top text-center py-3 py-lg-4">
 	<h1 class="display-huge"><?=buildFrontPageHeadline(get_bloginfo('name'));?></h1>
-</section>
+</header>
