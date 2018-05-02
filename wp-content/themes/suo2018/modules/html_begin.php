207a0a9ff4c350af4c46f82fdd18f357d76541ae
Sascha Metz <metz@raum3.de>
2018-05-02 10:27:18 +0200

    Pagetitle Update

--- wp-content/themes/suo2018/modules/html_begin.php
+++ wp-content/themes/suo2018/modules/html_begin.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?bloginfo('name');?></title>
+    <title><?bloginfo('name');?> | <?=!is_front_page() ? get_the_title() : 'Startseite';?></title>
     <?wp_head();?>
 </head>
 <body<?if(is_front_page())echo ' class="bg-std"';?>>
