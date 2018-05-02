a813a8c8f83a90794bb8563d5bb1cb278bb10d81
Sascha Metz <metz@raum3.de>
2018-05-02 11:25:56 +0200

    Use this keyword (with correct scoping) in front-page.js

--- wp-content/themes/suo2018/src/js/front-page.js
+++ wp-content/themes/suo2018/src/js/front-page.js
@@ -1,7 +1,7 @@
 const frontPage = (() => {
 	function setup(){
-		$('.fp-content-square').on('click',(event)=>{
-			const self = $(event.currentTarget);
+		$('.fp-content-square').on('click',function(){
+			const self = $(this);
 			if(!self.hasClass('off')){
 				fadeOverlay.show(true,()=>{
 					window.location.href = self.data('link');
