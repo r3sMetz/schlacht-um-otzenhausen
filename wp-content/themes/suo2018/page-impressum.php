793f8a7d3d54cf18311f8e78880d22e2cb88fc03
Sascha Metz <metz@raum3.de>
2018-05-10 18:00:45 +0200

    Page Impressum

---
+++ wp-content/themes/suo2018/page-impressum.php
@@ -0,0 +1,17 @@
+<?php
+//HTML Begin
+get_template_part('modules/html_begin');
+
+// Navbar Module
+get_template_part('modules/navbar');
+
+// Page Content
+get_template_part('partials/page-impressum/page-impressum_content.php');
+
+// Footer
+get_template_part('modules/footer');
+
+
+//HTML End
+get_template_part('modules/html_end');
+
---
+++ wp-content/themes/suo2018/partials/page-impressum/page-impressum_content.php
@@ -0,0 +1,12 @@
+<div class="page-impressum_content puffer-both-xl">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+                <?the_field('linke_seite');?>
+			</div>
+			<div class="col-sm-8 margin-top-mobile">
+                <?the_field('rechte_seite');?>
+            </div>
+		</div>
+	</div>
+</div>
