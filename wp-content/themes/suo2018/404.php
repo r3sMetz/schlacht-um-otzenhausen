327e91d4f8735cf88411232b63886221397ccd92
Sascha Metz <metz@raum3.de>
2018-05-26 12:23:42 +0200

    Decent 404

---
+++ wp-content/themes/suo2018/404.php
@@ -0,0 +1,17 @@
+<?php
+//HTML Begin
+get_template_part('modules/html_begin');
+
+// Navbar Module
+get_template_part('modules/navbar');
+
+// Page Info Content
+get_template_part('partials/404/404-content');
+
+// Footer
+get_template_part('modules/footer');
+
+
+//HTML End
+get_template_part('modules/html_end');
+
---
+++ wp-content/themes/suo2018/partials/404/404-content.php
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class="row py-5 mb-5">
+		<div class="col-12 text-center">
+			<h1 class="h3">Seite nicht gefunden</h1>
+			<img class="img-fluid" src="<?=get_template_directory_uri();?>/assets/img/404.gif" alt="CWO - 404">
+		</div>
+	</div>
+</div>
