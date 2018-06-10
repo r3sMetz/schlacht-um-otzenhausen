44cd09136c3958dba6f1184b8b2a1add85d3afc1
Sascha Metz <metz@raum3.de>
2018-06-10 16:30:07 +0200

    Removed Edit Page

--- wp-content/themes/suo2018/includes/clearBackend.php
+++ wp-content/themes/suo2018/includes/clearBackend.php
@@ -7,7 +7,7 @@ update_option( 'link_manager_enabled', 0 );
 
 
 function custom_menu_page_removing() {
-    //remove_menu_page('edit.php');
+    remove_menu_page('edit.php');
     remove_menu_page('edit-comments.php');
 }
 add_action( 'admin_menu', 'custom_menu_page_removing' );
