7294fd3f39cf416a7697f3ece9974fae7022c9b6
Sascha Metz <metz@raum3.de>
2018-06-10 15:04:28 +0200

    Some Comments

--- wp-content/themes/suo2018/partials/page-running-order/pro-content.php
+++ wp-content/themes/suo2018/partials/page-running-order/pro-content.php
@@ -27,6 +27,7 @@
 			<?php endforeach; ?>
         </div>
     </div>
+    <!-- Running Order PDF -->
     <div class="row">
         <div class="col-12">
             <a target="_blank" href="<?php echo get_template_directory_uri();?>/assets/pdf/suo_running_order.pdf" class="btn btn-secondary">Running Order als PDF</a>
