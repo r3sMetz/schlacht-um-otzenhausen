aedb5247c4d0beb9e044e24e0f0fa8cb11999cb8
Sascha Metz <metz@raum3.de>
2018-07-05 22:13:16 +0200

    404 Update

--- wp-content/themes/suo2018/partials/404/404-content.php
+++ wp-content/themes/suo2018/partials/404/404-content.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<div class="row py-5 mb-5">
 		<div class="col-12 text-center">
-			<h1 class="h3">Seite nicht gefunden</h1>
+			<h1 class="h3">Fear 0 Fear - Seite nicht gefunden</h1>
 			<img class="img-fluid" src="<?=get_template_directory_uri();?>/assets/img/404.gif" alt="CWO - 404">
 		</div>
 	</div>
