ced04b45389ae6263e5259666aa507852f6fb66a
Sascha Metz <metz@raum3.de>
2018-07-10 21:41:17 +0200

    Updated Footer

--- wp-content/themes/suo2018/modules/footer.php
+++ wp-content/themes/suo2018/modules/footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<div class="container py-4 d-md-flex text-center text-md-left">
 		<span>&copy; <?=date('Y');?> Celtic Warriors Otzenhausen e.V.</span><br class="d-md-none">
-		<span class="ml-auto"><a href="<?=get_permalink(14);?>">Impressum</a></span>
+		<span class="ml-auto"><a href="<?=get_permalink(14);?>">Impressum</a> | <a href="<?=get_permalink(175);?>">Datenschutz</a></span>
     </div>
 </footer>
