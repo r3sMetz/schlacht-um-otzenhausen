5c8dfa2f33f73a9583150618d1e33fbc12b60c7a
Sascha Metz <metz@raum3.de>
2018-07-26 20:38:56 +0200

    Added Title Attributes to Catch up Links

--- wp-content/themes/suo2018/partials/page-tickets/pt-content.php
+++ wp-content/themes/suo2018/partials/page-tickets/pt-content.php
@@ -4,8 +4,8 @@
 			<h1>Schlacht um Otzenhausen - Tickets</h1>
 			<p class="h3">Du wirst in <span id="ticket_secs"></span>s zu unserem Ticketshop geleitet.</p>
 			<button class="btn btn-secondary mr-2" id="abort_ticket">Abbrechen</button>
-			<button class="btn btn-secondary mr-2 catchUp" id="catchUp">Direkt zum Ticketshop</button>
-			<button class="btn btn-secondary catchUp" id="catchUpDirect">Direkt Eventim</button>
+			<button class="btn btn-secondary mr-2 catchUp" id="catchUp" title="Unser Ticketshop">Direkt zum Ticketshop</button>
+			<button class="btn btn-secondary catchUp" id="catchUpDirect" title="Eventim Veranstaltung">Direkt zu Eventim</button>
 		</div>
 	</div>
 </div>
