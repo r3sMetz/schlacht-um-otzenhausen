2e7cb15d9563377a3122698e1e02a8701ae27beb
Sascha Metz <metz@raum3.de>
2018-08-02 22:36:50 +0200

    Page Bands Semaintic

--- wp-content/themes/suo2018/partials/page-bands/pb-content.php
+++ wp-content/themes/suo2018/partials/page-bands/pb-content.php
@@ -1,26 +1,26 @@
 <?$bandarray=build_band_array();?>
-<div class="container py-4">
+<main class="container py-4">
     <!-- Headliners -->
     <div class="pb_headliners d-md-flex mx-auto align-items-center justify-content-between flex-wrap">
         <?foreach($bandarray['headliner'] as $band):?>
-        <div class="single_band mb-5 mb-md-2">
+        <article class="single_band mb-5 mb-md-2">
             <h2 class="d-none"><?=get_the_title($band->ID);?></h2>
             <a href="<?=get_permalink($band->ID);?>" class="fadeLink">
                 <img class="img-fluid pb-link" src="<?=get_field('logo',$band->ID)['url'];?>" alt="<?=$band->post_title;?>">
             </a>
-        </div>
+        </article>
         <?endforeach;?>
     </div>
 
     <!-- Normal Bands -->
     <div class="pb_normals d-flex mx-auto align-items-center justify-content-between flex-wrap">
         <?foreach($bandarray['normal'] as $band):?>
-        <div class="single_band mb-5 mb-md-2">
+        <article class="single_band mb-5 mb-md-2">
             <h3 class="d-none"><?=get_the_title($band->ID);?></h3>
             <a href="<?=get_permalink($band->ID);?>" class="fadeLink">
                 <img class="img-fluid pb-link" src="<?=get_field('logo',$band->ID)['url'];?>" alt="<?=$band->post_title;?>">
             </a>
-        </div>
+        </article>
         <?endforeach;?>
     </div>
-</div>
+</main>
