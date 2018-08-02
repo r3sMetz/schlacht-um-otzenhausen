0730d4f4e401578082f512430591ec804d98bbbe
Sascha Metz <metz@raum3.de>
2018-08-02 22:33:07 +0200

    Page Anfahrt Semantic

--- wp-content/themes/suo2018/partials/page-anfahrt/pa-content.php
+++ wp-content/themes/suo2018/partials/page-anfahrt/pa-content.php
@@ -1,3 +1,3 @@
-<div class="container">
+<main class="container">
 	<iframe width="640" height="360" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2506.2081260999494!2d6.993172375878941!3d49.605053882734275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47959061a88760ed%3A0x4bda0943f72e1c1e!2sRingwallstra%C3%9Fe+8%2C+66620+Nonnweiler!5e0!3m2!1sde!2sde!4v1472397361861"></iframe>
-</div>
+</main>
