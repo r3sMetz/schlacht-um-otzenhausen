3a2947069702e2e670d2d6bb2de3176678dac029
Sascha Metz <metz@raum3.de>
2018-09-03 06:31:14 +0200

    Updated Page Tickets

--- wp-content/themes/suo2018/partials/page-tickets/pt-content.php
+++ wp-content/themes/suo2018/partials/page-tickets/pt-content.php
@@ -3,9 +3,9 @@
 		<div class="col-12">
 			<h1>Schlacht um Otzenhausen - Tickets</h1>
 			<p class="h3">Du wirst in <span id="ticket_secs"></span>s zu unserem Ticketshop geleitet.</p>
-			<button class="btn btn-secondary mr-2" id="abort_ticket">Abbrechen</button>
-			<button class="btn btn-secondary mr-2 catchUp" id="catchUp" title="Unser Ticketshop">Direkt zum Ticketshop</button>
-			<button class="btn btn-secondary catchUp" id="catchUpDirect" title="Eventim Veranstaltung">Direkt zu Eventim</button>
+			<button class="btn btn-secondary mr-2 mb-3 mt-4 mt-sm-0" id="abort_ticket">Abbrechen</button>
+			<button class="btn btn-secondary mr-2 mb-3 catchUp" id="catchUp" title="Unser Ticketshop">Direkt zum Ticketshop</button>
+			<button class="btn btn-secondary catchUp mb-3" id="catchUpDirect" title="Eventim Veranstaltung">Direkt zu Eventim</button>
 		</div>
 	</div>
 </main>
