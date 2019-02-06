66655cb94aef8a17a382a25abc2d6a1a41e26966
Sascha Metz <metz@raum3.de>
2019-02-06 19:48:26 +0100

    Adding Lazyloaded to body on callback_finish()

--- wp-content/themes/suo2018/src/js/lazy-loading.js
+++ wp-content/themes/suo2018/src/js/lazy-loading.js
@@ -1,7 +1,10 @@
 const lazyLoading = (function(){
 	function setup() {
 		const myLazyLoad = new LazyLoad({
-    		elements_selector: ".iam-lazy"
+    		elements_selector: ".iam-lazy",
+    		callback_finish: () => {
+    			document.body.classList.add('all-lazy-loaded')
+    		}
 		});
 	}
 
