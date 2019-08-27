56080e7f9379e11309d92bc50f01f6f4d5d67995
Sascha Metz <metz@raum3.de>
2019-08-27 14:00:04 +0200

    Removing metal.de from fp-footer.php

--- wp-content/themes/suo2018/partials/front-page/fp-footer.php
+++ wp-content/themes/suo2018/partials/front-page/fp-footer.php
@@ -6,8 +6,3 @@
 <nav id="privacy_imprint" class="text-center text-md-left position-fixed suo_footer_btm">
     <a class="text-light fadeLink" href="<?php echo get_permalink(14);?>">Impressum</a>&nbsp;|&nbsp;<a class="text-light fadeLink" href="<?php echo get_permalink(175);?>">Datenschutz</a>
 </nav>
-
-<!-- metal.de -->	
-<a href="https://www.metal.de/" target="_blank" id="metalde" rel="noopener" class="position-fixed suo_footer_btm">	
-    <img src="<?php echo get_theme_file_uri('assets/img/metalde.png');?>" alt="metal.de">	
-</a>
