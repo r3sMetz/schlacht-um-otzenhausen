0b0721fbafd1f9b29e488bf54a6053244135239f
Sascha Metz <metz@raum3.de>
2019-08-27 13:55:11 +0200

    Removing support banner

--- wp-content/themes/suo2018/partials/front-page/fp-content.php
+++ wp-content/themes/suo2018/partials/front-page/fp-content.php
@@ -1,4 +1,4 @@
-<a id="supporter_banner" class="bg-secondary text-white fadeLink" href="<?php echo get_permalink(171);?>">Support us!</a>
+<!-- <a id="supporter_banner" class="bg-secondary text-white fadeLink" href="<?php echo get_permalink(171);?>">Support us!</a> -->
 <main class="fp-content d-flex flex-wrap text-center mx-auto">
     <?php foreach(r3_getMenue('Hauptmenu',get_the_ID()) as $item):?>
         <?php if($item->object_id != 118):?>
